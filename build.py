e7104ce38929133a48365b83192afc7b12903955
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-02 18:01:47 -0500

    Fix build

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_types=["Release"])
+    remotes = [("https://api.bintray.com/conan/appimage-conan-community/public-conan", "true", "appimage"),
+               ("https://api.bintray.com/conan/bincrafters/public-conan", "true", "bincrafters")]
+    builder = ConanMultiPackager(remotes=remotes, build_policy="outdated", build_types=["Release"])
     builder.add_common_builds()
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -11,8 +11,19 @@ class AppimagetoolinstallerConan(ConanFile):
     description = "Package desktop applications as AppImages that run on common Linux-based operating systems, such as RHEL, CentOS, openSUSE, SLED, Ubuntu, Fedora, debian and derivatives."
     topics = ("AppImage", "Creation", "Tool")
     settings = "os", "compiler", "build_type", "arch"
+    requires = ("zlib/1.2.11@conan/stable")
+    keep_imports = True
+
+    def configure(self):
+        self.options["zlib"].shared = True
+
+    def imports(self):
+        self.copy("libz*.so*", src="lib", dst="squashfs-root/usr/lib")
 
     def build(self):
+        if (self.settings.build_type != "Release"):
+            raise errors.ConanInvalidConfiguration("Only Release builds are supported.")
+
         if (self.settings.arch == "x86" or self.settings.arch == "x86_64"):
             # match the arch naming convention used in AppImageKit
             appimagetool_arch_name = "i686" if self.settings.arch == "x86" else self.settings.arch
@@ -22,10 +33,8 @@ class AppimagetoolinstallerConan(ConanFile):
         else:
             raise errors.ConanInvalidConfiguration("Unsuported arch: %s" % self.settings.arch)
 
-        self.run("chmod +x appimagetool-x86_64.AppImage && ./appimagetool-x86_64.AppImage --appimage-extract")
-
-        if (self.settings.build_type != "Release"):
-            raise errors.ConanInvalidConfiguration("Only Release builds are supported.")
+        self.run("chmod +x appimagetool-x86_64.AppImage && ./appimagetool-x86_64.AppImage --appimage-extract",
+                 run_environment=True)
 
     def package(self):
         self.copy("*", dst="", src="squashfs-root/usr")
